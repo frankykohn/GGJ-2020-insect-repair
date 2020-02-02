3ed34f8fad542ddefe50d70491d2648ecfc75236
Franky Kohn <franky.kohn@gmail.com>
2020-02-01 16:28:20 -0800

    Sound Triggers (#1)
    * add audio assets
    
    * add assets
    
    * get sound to work in background with actions

---
+++ SoundTriggers.gd
@@ -0,0 +1,15 @@
+extends Node2D
+
+func _ready():
+	$MainMusic.play()
+	pass
+		
+func _input(event):
+	if(event.is_action_pressed("combo_key_1")):
+		$JSound.play()
+	if(event.is_action_pressed("combo_key_2")):
+		$KSound.play()
+	if(event.is_action_pressed("combo_key_3")):
+		$LSound.play()
+	
+	
