db79b96b69f814cc436345382b64d3d3b9ae0a6d
Christopher Chan <christopher@Christophers-MacBook-Pro.local>
2020-02-02 15:44:33 -0800

    adds popup to endgame

--- Mother.gd
+++ Mother.gd
@@ -1,13 +1,20 @@
 extends Node2D
+var popUp
 
+func _ready():
+	popUp = get_node("../End")
+	pass
 
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-var speed = 0.00025
+var speed = 0.0005
 var location = 0
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	get_node("Mother_Path/PathFollow2D").set_unit_offset(location)
-	location += speed
+	if location >= 1:
+		popUp.show()
+	else:
+		get_node("Mother_Path/PathFollow2D").set_unit_offset(location)
+		location += speed
--- SoundTriggers.gd
+++ SoundTriggers.gd
@@ -1,9 +1,17 @@
 extends Node2D
+var popUp
+var insect
+var mother
+var planet
 
 func _ready():
 	$MainMusic.play()
+	popUp = get_node("End")
+	insect = get_node("Insect")
+	mother = get_node("Mother")
+	planet = get_node("Planet")
 	pass
-		
+
 func _input(event):
 	if(event.is_action_pressed("combo_key_1")):
 		$JSound.play()
@@ -11,5 +19,10 @@ func _input(event):
 		$KSound.play()
 	if(event.is_action_pressed("combo_key_3")):
 		$LSound.play()
-	
-	
+		popUp.show()
+
+func _on_PlayAgain_pressed():
+	get_tree().change_scene('res://Gameplay.tscn')
+
+func _on_MainMenu_pressed():
+	get_tree().change_scene('res://MenuScreen.tscn')
